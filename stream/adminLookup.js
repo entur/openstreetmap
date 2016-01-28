ae9192cdb93d3de4542d357a441e74572002c81a
Cristoffer Solem <cristoffer.solem@netlight.com>
2016-01-28 11:16:18 +0100

    Attempt to fall back to pelias admin lookup if URL for WofPipResolver is not configured

--- stream/adminLookup.js
+++ stream/adminLookup.js
@@ -1,6 +1,7 @@
 var through = require('through2');
 var peliasConfig = require( 'pelias-config' ).generate();
 var wofAdminLookup = require('pelias-wof-admin-lookup');
+var peliasAdminLookup = require('pelias-admin-lookup')
 
 /**
  * Generate a stream object that will handle the adminLookup when enabled.
@@ -14,11 +15,18 @@ function createStream(config, adminLookup) {
   config = config || peliasConfig;
   adminLookup = adminLookup || wofAdminLookup;
 
-  if (config.imports.openstreetmap.adminLookup) {
+  if (config.imports.openstreetmap.adminLookup
+    && config.imports.adminLookup
+    && config.imports.adminLookup.url) {
+
     var pipResolver = adminLookup.createWofPipResolver(config.imports.adminLookup.url);
     return adminLookup.createLookupStream(pipResolver);
-  }
-  else {
+
+  } else if(config.imports.openstreetmap.adminLookup) {
+
+    return peliasAdminLookup.stream();
+
+  } else {
     return through.obj(function (doc, enc, next) {
       next(null, doc);
     });
